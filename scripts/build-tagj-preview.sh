c89e0cbbd3056cee46f92b843e8584dfebf4982f
BennyBundles <146196050+BennyBundles@users.noreply.github.com>
2026-09-19 17:35:50 -0400

    Ship legal drafts and bump TAGJ preview to V15.9

--- scripts/build-tagj-preview.sh
+++ scripts/build-tagj-preview.sh
@@ -2,12 +2,13 @@
 set -eu
 
 rm -rf .tagj-dist
-mkdir -p .tagj-dist/tagj-assets/v147 .tagj-dist/tagj-assets/intro .tagj-dist/tagj-data
+mkdir -p .tagj-dist/tagj-assets/v147 .tagj-dist/tagj-assets/intro .tagj-dist/tagj-data .tagj-dist/legal
 
 cp index.html full.html tagj.html artist.html producer.html creative.html .tagj-dist/
 cp tagj-assets/*.css .tagj-dist/tagj-assets/
 cp tagj-assets/v147/* .tagj-dist/tagj-assets/v147/
 cp tagj-assets/intro/* .tagj-dist/tagj-assets/intro/
 cp tagj-data/* .tagj-dist/tagj-data/
+cp legal/* .tagj-dist/legal/
 
-printf 'V15.8' > .tagj-dist/VERSION.txt
+printf 'V15.9' > .tagj-dist/VERSION.txt
