281ba28aac7a948bf045b95adde6a1720331e373
BennyBundles <146196050+BennyBundles@users.noreply.github.com>
2026-09-19 00:04:40 -0400

    Add TAGJ preview build script

---
+++ scripts/build-tagj-preview.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -eu
+
+rm -rf .tagj-dist
+mkdir -p .tagj-dist/tagj-assets .tagj-dist/tagj-data
+
+cp index.html full.html tagj.html artist.html producer.html creative.html .tagj-dist/
+cp tagj-assets/*.css .tagj-dist/tagj-assets/
+cp tagj-data/* .tagj-dist/tagj-data/
+
+printf 'V14.4' > .tagj-dist/VERSION.txt
