7767b71edc113a1fc0019e8d76be58e53ad5b5d2
BennyBundles <146196050+BennyBundles@users.noreply.github.com>
2026-09-19 12:02:28 -0400

    V14.9 include intro video and poster in Vercel build

--- scripts/build-tagj-preview.sh
+++ scripts/build-tagj-preview.sh
@@ -2,11 +2,12 @@
 set -eu
 
 rm -rf .tagj-dist
-mkdir -p .tagj-dist/tagj-assets/v147 .tagj-dist/tagj-data
+mkdir -p .tagj-dist/tagj-assets/v147 .tagj-dist/tagj-assets/intro .tagj-dist/tagj-data
 
 cp index.html full.html tagj.html artist.html producer.html creative.html .tagj-dist/
 cp tagj-assets/*.css .tagj-dist/tagj-assets/
 cp tagj-assets/v147/* .tagj-dist/tagj-assets/v147/
+cp tagj-assets/intro/* .tagj-dist/tagj-assets/intro/
 cp tagj-data/* .tagj-dist/tagj-data/
 
 printf 'V14.9' > .tagj-dist/VERSION.txt
